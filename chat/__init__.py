19fd905bf543d08578bf9bdbe1878fbf1400c8a6
alzhang12 <alzhang@umich.edu>
2020-08-22 15:05:40 -0400

    added DB

--- chat/__init__.py
+++ chat/__init__.py
@@ -1,10 +1,34 @@
 """Chat package initializer."""
-import flask
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+from flask_heroku import Heroku
 
 # define flask app
-app = flask.Flask(__name__)
+app = Flask(__name__)
 
-# config (maybe we'll do this later when we need it)
+# configuration
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://localhost/users'
+heroku = Heroku(app)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+# table
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(), unique=True)
+    password = db.Column(db.String(), unique=True)
+
+    def __init__(self, username, password):
+        """Enter a new user."""
+        self.username = username
+        self.password = password
+
+    def __repr__(self):
+        """Representation."""
+        return '<id %r>' % self.id
+
 # circular imports!
 import chat.api
 import chat.views
--- chat/views/render.py
+++ chat/views/render.py
@@ -2,8 +2,16 @@
 import flask
 import chat
 
-@chat.app.route('/')
+@chat.app.route('/', methods=['GET', 'POST'])
 def show_index():
     """Render main page."""
+    if flask.request.method == 'POST':
+        username = flask.request.form['username']
+        password = flask.request.form['password']
+    
+        new_user = chat.User(username, password)
+        chat.db.session.add(new_user)
+        chat.db.session.commit()
+    
     context = {}
     return flask.render_template("index.html", **context)
